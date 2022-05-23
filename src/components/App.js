eeb770f1d456b1d7d45069fa2e9a9636b094e0e6
QinruoYang <qy54@uw.edu>
2022-05-22 22:57:08 -0700

    update routing

--- src/components/App.js
+++ src/components/App.js
@@ -8,25 +8,14 @@ import About from './About';
 
 function App() {
   return (
-    <body>
+    <div>
       <NavBar />
-      {/* <Router>
-        <NavBar />
-        <Routes>
-          <Route path='' exact component={History} />
-          <Route path='/PlayGallery' component={PlayGallery} />
-          <Route path='/About' component={About} />
-        </Routes>
-      </Router> */}
-
-      <header>
-        <h1>Chinese Theater Club</h1>
-      </header>
-    <main>
-      <History />
-    </main>
-    <footer>&copy; Copyright 2022 Chinese Theater Club</footer>
-    </body>
+      <Routes>
+        <Route path="/" element={<History />} />
+        <Route path='/PlayGallery' element={<PlayGallery />} />
+        <Route path='/About' element={<About />} />
+      </Routes>
+    </div>
   );
 }
 
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,22 +3,22 @@ import {  Link } from "react-router-dom";
 
 const NavBar= () =>{
   return (
-  // <div>
-  //   <li>
-  //     <Link to="/">History</Link>
-  //   </li>
-  //   <li>
-  //     <Link to="/PlayGallery">PlayGallery</Link>
-  //   </li>
-  //   <li>
-  //     <Link to="/About">About</Link>
-  //   </li>
-  // </div>
-  <nav>
-    <a  href="./History.js">Home</a>
-    <a href="./PlayGallery.js">Plays</a>
-    <a href="./About.js">About</a>
-  </nav>
+  <div>
+    <li>
+      <Link to="/">History</Link>
+    </li>
+    <li>
+      <Link to="/PlayGallery">PlayGallery</Link>
+    </li>
+    <li>
+      <Link to="/About">About</Link>
+    </li>
+  </div>
+  // <nav>
+  //   <a  href="./History.js">Home</a>
+  //   <a href="./PlayGallery.js">Plays</a>
+  //   <a href="./About.js">About</a>
+  // </nav>
   );
 }
 export default NavBar;
