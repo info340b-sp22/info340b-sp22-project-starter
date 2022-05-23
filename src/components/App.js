4c797f48f6b24c61753714e527b54155f75a4ce8
QinruoYang <qy54@uw.edu>
2022-05-23 12:17:02 -0700

    revert

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import History from './History';
 import NavBar from './Navigation';
 import PlayGallery from './PlayGallery';
-import Chart from './Chart'
-import About from './About'
-
+import About from './About';
 
 
 function App() {
@@ -14,7 +12,6 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<History />} />
-        <Chart />
         <Route path='/PlayGallery' element={<PlayGallery />} />
         <Route path='/About' element={<About />} />
       </Routes>
