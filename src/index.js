ff4fc1495fbc67d849aa7e5009e249c6abdbec77
angsoenW <91903037+angsoenW@users.noreply.github.com>
2022-05-30 16:35:25 -0700

    Revert "Merge branch 'main' of https://github.com/info340b-sp22/info340b-sp22-project-starter"
    This reverts commit 9c9e2b5d53e4a127c42409f4105f9db038d41814, reversing
    changes made to b8af7793f1d1f45966e71e98be75b80ad9729894.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,27 @@ import './index.css';
 import App from './components/App';
 import historyData from "./data/historyContent.json";
 import contactInfo from "./data/contact.json";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAlHN1kd0Pqq_QdZi7aoSmkRl3tGoKstrY",
+  authDomain: "ctc-official.firebaseapp.com",
+  projectId: "ctc-official",
+  storageBucket: "ctc-official.appspot.com",
+  messagingSenderId: "549727310344",
+  appId: "1:549727310344:web:1345762ae87207926777bd",
+  measurementId: "G-WFKXJB83T5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 
 
