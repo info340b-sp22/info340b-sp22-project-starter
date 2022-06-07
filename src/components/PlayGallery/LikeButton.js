794650029986a8879cd14236b03d7d4b02331673
angsoenW <91903037+angsoenW@users.noreply.github.com>
2022-06-07 15:53:28 -0700

    Update LikeButton.js

--- src/components/PlayGallery/LikeButton.js
+++ src/components/PlayGallery/LikeButton.js
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 
+// Likebutton button implementation from https://stackblitz.com/edit/like-button-component-using-react-16?file=src%2FApp.js
 //a like button that user can use to interact with the website.
 const LikeButton = () => {
-  const [like, setLike] = useState(Math.floor(Math.random() * 1000)), // used random number of likes for each play since we don't know how to edit json file when user interact with the website.
+  const randomLimit = 1000;
+  const [like, setLike] = useState(Math.floor(Math.random() * randomLimit)), // used random number of likes for each play since we don't know how to edit json file when user interact with the website.
     [isLike, setIsLike] = useState(false),
     onLikeButtonClick = () => {
       setLike(like + (isLike?-1:1));
