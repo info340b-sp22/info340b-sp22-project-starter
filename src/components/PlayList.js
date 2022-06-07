4d618ce61c725ecf36d659f7fbada99fdef65d14
QinruoYang <qy54@uw.edu>
2022-06-07 13:10:53 -0700

    fix some style issue

--- src/components/PlayList.js
+++ src/components/PlayList.js
@@ -4,28 +4,35 @@ import LikeButton from './LikeButton';
 
 export default function PlayList(props) {
   const [searchTerm, setSearchTerm] = useState("");
+
+  let searchInput =
+  <div className="searchInput_Container">
+    <input id="searchInput" type="text" placeholder="Search here..." onChange={(event) => {
+      setSearchTerm(event.target.value);
+    }}/>
+  </div>
+
+  let display =
+    <div>
+      {
+        props.plays
+          .filter((val) => {
+            if(searchTerm === ""){
+              return true;
+            }else if(val.name.toLowerCase().includes(searchTerm.toLowerCase())){
+              return true;
+            }else {
+              return false;
+            }
+          })
+          .map((val) => <GenButton plays={val} key={val.name}/>)
+      }
+    </div>
+
   return (
     <section className='plays'>
-      <div className="searchInput_Container">
-        <input id="searchInput" type="text" placeholder="Search here..." onChange={(event) => {
-          setSearchTerm(event.target.value);
-        }}/>
-      </div>
-      <div>
-          {
-            props.plays
-              .filter((val) => {
-                if(searchTerm === ""){
-                  return true;
-                }else if(val.name.toLowerCase().includes(searchTerm.toLowerCase())){
-                  return true;
-                }else {
-                  return false;
-                }
-              })
-              .map((val) => <GenButton plays={val} key={val.name}/>)
-          }
-        </div>
+      {searchInput}
+      {display}
     </section>
   );
 }
