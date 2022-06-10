372f161283fd38eb884166ec91ee1a542079eaa9
QinruoYang <qy54@uw.edu>
2022-06-09 21:54:13 -0700

    update about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,23 +1,5 @@
 import React from 'react';
 
-function ContactCard(props) {
-  let {name, motto, img, email, title} = props.contact;
-  return (
-    <div className="column">
-      <div className="card">
-        <img src={img} alt={name} />
-        <div className="about-container">
-          <h2>{name}</h2>
-          <p className="title">{title}</p>
-          <p>{motto}</p>
-          <p>{email}</p>
-          <p><button className="button">Contact</button></p>
-        </div>
-      </div>
-    </div>
-  );
-}
-
 export default function About(props) {
   let contactArr = props.contact.map((ele) => <ContactCard contact={ele} key={ele.name} />);
   return (
@@ -36,6 +18,23 @@ export default function About(props) {
       <h3>Our Team</h3>
       <div className="about-row">
         {contactArr}
+      </div>
+    </div>
+  );
+}
+
+function ContactCard(props) {
+  let {name, motto, img, email, title} = props.contact;
+  return (
+    <div className="column">
+      <div className="card">
+        <img src={img} alt={name} />
+        <div className="about-container">
+          <h2>{name}</h2>
+          <p className="title">{title}</p>
+          <p>{motto}</p>
+          <p>{email}</p>
+        </div>
       </div>
     </div>
   );
